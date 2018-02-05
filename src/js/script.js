c3fa011d1f781ebc4e50a221671add8df69b60a7
LukaszMusial <qkm@op.pl>
2018-02-05 22:50:56 +0100

    styling in scss pt11

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,39 @@
-var person = {
-	name: "Marcin",
-	age: 33
-};
+const images = [
+  {
+    id: "photo_16",
+    src: "img/photo_15.jpg"
+  },
+  {
+    id: "photo_17",
+    src: "img/photo_16.jpg"
+  },
+  {
+    id: "photo_18",
+    src: "img/photo_17.jpg"
+  },
+  {
+    id: "photo_19",
+    src: "img/photo_18.jpg"
+  }
+];
 
-console.log(person.name);
+function imgTemplate (img) {
+  return `
+    <figure class="gallery__thumbnail img-photo-11">
+      <img src="${img.src}" alt="business cards" id="${img.id}" class="gallery__thumbnail__img">
+      <div class="wrapper-thumbnail__description">
+          <figcaption class="gallery__thumbnail__description">
+              <h2 class="gallery__thumbnail__header">REALISTIC BOOK</h2>
+              <p class="gallery__thumbnail__text">Print</p>
+              <button class="btn btn--secondary btn--modifer-lines btn--gallery right">Wiew work</button>
+          </figcaption>
+      </div>
+    </figure>`
+}
+
+var button = document.querySelector("#button_load");
+button.addEventListener("click", function() {
+  var gallery = document.querySelector(".gallery");
+  // gallery.insertAdjacentHTML('afterend', images.map(imgTemplate));
+  // gallery.append(`${HTML.stringify(images.map(imgTemplate).join(""))}`);
+});
