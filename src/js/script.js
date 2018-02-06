2f8c340fa78944836f405e5da21c6292142427c7
LukaszMusial <qkm@op.pl>
2018-02-06 16:39:03 +0100

    load more gallery in js

--- src/js/script.js
+++ src/js/script.js
@@ -17,9 +17,14 @@ const images = [
   }
 ];
 
+var count = 0;
+var button = document.querySelector("#button_load");
+var gallery = document.querySelector(".gallery");
+// var parser = new DOMParser(); 
+
 function imgTemplate (img) {
-  return `
-    <figure class="gallery__thumbnail img-photo-11">
+  count++;
+  return  `<figure class="gallery__thumbnail">
       <img src="${img.src}" alt="business cards" id="${img.id}" class="gallery__thumbnail__img">
       <div class="wrapper-thumbnail__description">
           <figcaption class="gallery__thumbnail__description">
@@ -29,11 +34,62 @@ function imgTemplate (img) {
           </figcaption>
       </div>
     </figure>`
-}
 
-var button = document.querySelector("#button_load");
+
+
+};
+
+
+
+
+
 button.addEventListener("click", function() {
-  var gallery = document.querySelector(".gallery");
+  if(count < images.length) {
+    // gallery.innerHTML += `${images.map(imgTemplate).join("")}`;
+    // gallery.append(`${images.map(imgTemplate).join("")}`);
+    // gallery.append(imgTemplate);
+    gallery.insertAdjacentHTML('afterend', imgTemplate);
+
+    button.classList.add("hide");
+  }
+  else {
+    // button.classList.add("hide");
+  }
+  // var blankDiv = document.createElement("div");
   // gallery.insertAdjacentHTML('afterend', images.map(imgTemplate));
-  // gallery.append(`${HTML.stringify(images.map(imgTemplate).join(""))}`);
+  // gallery.append(`${images.map(imgTemplate).join("")}`);
+  // blankDiv.innerText = `${images.map(imgTemplate)}`;
+  // gallery.appendChild(blankDiv);
+
 });
+
+
+
+
+  // var figure = document.createElement("figure");
+    // figure.classList.add("gallery__thumbnail");
+    // // var ParentDiv = document.querySelector(".gallery");
+    // // ParentDiv.appendChild(figure);
+    // var image = document.createElement("img");
+    // image.id = 'img.id';
+    // image.classList.add('gallery__thumbnail__img');
+    // image.setAttribute('src', 'img/photo_14.jpg');
+    // image.setAttribute('alt', 'business cards');
+    // figure.appendChild(image);
+    // var div = document.createElement("div");
+    // div.classList.add('wrapper-thumbnail__description');
+    // figure.appendChild(div);
+    // var figcaption = document.createElement("figcaption");
+    // figcaption.classList.add('gallery__thumbnail__description');
+    // div.appendChild(figcaption);
+    // var header = document.createElement("h2");
+    // header.classList.add('gallery__thumbnail__header');
+    // header.innerText = 'REALISTIC BOOK';
+    // figcaption.appendChild(header);
+    // var paragraph = document.createElement("p");
+    // paragraph.classList.add('gallery__thumbnail__text');
+    // paragraph.innerText = 'Print';
+    // figcaption.appendChild(paragraph);
+    // var addButton = document.createElement("button");
+    // addButton.classList.add('btn');
+    // paragraph.innerText = 'Wiew work';
