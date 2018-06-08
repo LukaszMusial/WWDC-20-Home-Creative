454c0a79ee21a4b0d77a386d02a19ac4d1e572a4
LukaszMusial <qkm@op.pl>
2018-06-08 16:57:05 +0200

    google maps

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -1,1 +1,16 @@
-// console.log('testujemy');
+ var map;
+
+function initMap() {
+
+	var uluru = {lat: 50.0646501, lng: 19.9449799};
+
+	map = new google.maps.Map(document.getElementById('map'), {
+	  center: uluru,
+	  zoom: 10
+	});
+	var marker = new google.maps.Marker({
+		position: uluru,
+		map: map
+	});
+	
+}
